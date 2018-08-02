5c78f980bd49ca771aacea5ee0d5de980f53d16e
lalet <laletscaria@yahoo.co.in>
2018-08-02 13:22:49 -0400

    Edited wrapper

--- eddy_wrapper.sh
+++ eddy_wrapper.sh
@@ -6,46 +6,55 @@ LD_LIBRARY_PATH=""
 for i in "$@"
 do
 case $i in
-    --imain=*)
-    COMMAND_LINE="--imain=${i#*=}"
+    --imain)
+    COMMAND_LINE="$2"
     shift # past argument
+    shift
     ;;
-    --mask=*)
-    COMMAND_LINE="${COMMAND_LINE} --mask=${i#*=}"
+    --mask)
+    COMMAND_LINE="${COMMAND_LINE} --mask="$2""
     shift # past argument
+    shift
     ;;
-    --index=*)
-    COMMAND_LINE="${COMMAND_LINE} --index=${i#*=}"
+    --index)
+    COMMAND_LINE="${COMMAND_LINE} --index="$2""
     shift # past argument
+    shift
     ;;
-    --acqp=*)
-    COMMAND_LINE="${COMMAND_LINE} --acqp=${i#*=}"
+    --acqp)
+    COMMAND_LINE="${COMMAND_LINE} --acqp="$2""
     shift # past argument
+    shift
     ;;
-    --bvecs=*)
-    COMMAND_LINE="${COMMAND_LINE} --bvecs=${i#*=}"
+    --bvecs)
+    COMMAND_LINE="${COMMAND_LINE} --bvecs="$2""
     shift # past argument
+    shift
     ;;
-    --bvals=*)
-    BVALS="${i#*=}"
-    COMMAND_LINE="${COMMAND_LINE} --bvals=${i#*=}"
+    --bvals)
+    COMMAND_LINE="${COMMAND_LINE} --bvals="$2""
     shift # past argument
+    shift
     ;;
-    --fwhm=*)
-    COMMAND_LINE="${COMMAND_LINE} --fwhm=${i#*=}"
+    --fwhm)
+    COMMAND_LINE="${COMMAND_LINE} --fwhm="$2""
     shift # past argument
+    shift
     ;;
-    --topup=*)
-    COMMAND_LINE="${COMMAND_LINE} --topup=${i#*=}"
+    --topup)
+    COMMAND_LINE="${COMMAND_LINE} --topup="$2""
     shift # past argument
+    shift
     ;;
-    --flm=*)
-    COMMAND_LINE="${COMMAND_LINE} --flm=${i#*=}"
+    --flm)
+    COMMAND_LINE="${COMMAND_LINE} --flm="$2""
     shift # past argument
+    shift
     ;;
-    --out=*)
-    COMMAND_LINE="${COMMAND_LINE} --out=${i#*=}"
+    --out)
+    COMMAND_LINE="${COMMAND_LINE} --out="$2""
     shift # past argument
+    shift
     ;;
     -v)
     COMMAND_LINE="${COMMAND_LINE} -v"
@@ -68,5 +77,5 @@ echo "${FSLDIR}"
 source ${FSLDIR}/etc/fslconf/fsl.sh
 export PATH=/home/shawn.mcin/apps/cuda8.0/bin:/home/shawn.mcin/eddy/host_files/bin:${FSLDIR}/bin:$PATH
 export LD_LIBRARY_PATH=/home/shawn.mcin/apps/cuda8.0/lib64:/home/shawn.mcin/eddy/host_files/lib64:$LD_LIBRARY_PATH
-#echo COMMAND_LINE = "${COMMAND_LINE}"
+echo COMMAND_LINE = "${COMMAND_LINE}"
 eddy_cuda8.0 ${COMMAND_LINE}
